aa7f58c422aeb6ca70a51e3e3c2d8953da8f889e
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-19 16:12:41 +0400

    mui

--- Client/reactShop/src/app/layout/Navbar.tsx
+++ Client/reactShop/src/app/layout/Navbar.tsx
@@ -1,5 +1,11 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
-
-export default function NavBar{
-    
+export default function Navbar() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6">My Wine Shop</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
