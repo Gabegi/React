0a495994266815de399baa72f8cd61a1bfee7166
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-20 18:13:00 +0400

    layout product

--- Client/reactShop/src/features/products/ProductCard.tsx
+++ Client/reactShop/src/features/products/ProductCard.tsx
@@ -17,18 +17,20 @@ export default function ProductCard({ product }: Props) {
     <Card>
       <CardMedia
         component="img"
-        height="140"
+        height="240"
         image={product.pictureUrl}
         alt={product.name}
+        sx={{
+          objectFit: "cover", // Ensures images fill the frame
+        }}
       />
       <CardContent>
-        <Typography gutterBottom variant="h6">
-          {product.name}
-        </Typography>
-        <Typography>${product.price.toFixed(2)}</Typography>
+        <Typography variant="h6">{product.name}</Typography>
+        <Typography color="text.secondary">{product.description}</Typography>
+        <Typography sx={{ mt: 1 }}>${product.price.toFixed(2)}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to cart</Button>
+        <Button size="small">Add to Cart</Button>
         <Button size="small">Details</Button>
       </CardActions>
     </Card>
