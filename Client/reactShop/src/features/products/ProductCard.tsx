899a78c601a25095302d830aef18434a9e6dfeb8
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-20 18:40:00 +0400

    layout product

--- Client/reactShop/src/features/products/ProductCard.tsx
+++ Client/reactShop/src/features/products/ProductCard.tsx
@@ -1,10 +1,11 @@
 import {
   Card,
-  CardMedia,
   CardContent,
+  CardMedia,
   Typography,
   CardActions,
   Button,
+  Box,
 } from "@mui/material";
 import type { Product } from "../../app/models/product";
 
@@ -14,21 +15,32 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="240"
-        image={product.pictureUrl}
-        alt={product.name}
-        sx={{
-          objectFit: "cover", // Ensures images fill the frame
-        }}
-      />
-      <CardContent>
-        <Typography variant="h6">{product.name}</Typography>
-        <Typography color="text.secondary">{product.description}</Typography>
-        <Typography sx={{ mt: 1 }}>${product.price.toFixed(2)}</Typography>
+    <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+      <Box sx={{ height: 200, overflow: "hidden" }}>
+        <CardMedia
+          component="img"
+          image={product.pictureUrl}
+          alt={product.name}
+          sx={{
+            height: "100%",
+            width: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </Box>
+
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" gutterBottom>
+          {product.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.description}
+        </Typography>
+        <Typography sx={{ mt: 1 }} fontWeight="bold">
+          ${product.price.toFixed(2)}
+        </Typography>
       </CardContent>
+
       <CardActions>
         <Button size="small">Add to Cart</Button>
         <Button size="small">Details</Button>
