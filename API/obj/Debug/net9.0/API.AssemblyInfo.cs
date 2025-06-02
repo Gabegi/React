c0033eb4170a85b8c9fcecab9c194f82857b08b5
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-02 11:29:21 +0500

    API

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38e01ef08b5aa5debae56e69a90a4e7a2e51dfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcbf4fd40a4b69ad85212abd274343c8d219a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
