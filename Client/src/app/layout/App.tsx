2a61f0ecd89f21a9eb2ca0a9cff5fa8354565e5b
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-02 10:23:31 +0500

    plugging api

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,8 +1,26 @@
 import { Container } from "@mui/material";
 import ProductList from "../features/products/ProductList";
 import Navbar from "./Navbar";
+import { Product } from "../models/product";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://localhost:7004/api/Product")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        return response.json();
+      })
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <Navbar />
