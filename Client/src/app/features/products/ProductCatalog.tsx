9416f49c0f7cb48064a1a3ae99cc7999e3a4edfa
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-04 13:19:31 +0500

    routing

--- Client/src/app/features/products/ProductCatalog.tsx
+++ Client/src/app/features/products/ProductCatalog.tsx
@@ -1,19 +1,36 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../../models/product";
 import ProductList from "./ProductList";
+import Navbar from "../../layout/Navbar";
+import { Container } from "@mui/material";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://localhost:7010/api/Products/")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    fetch("https://localhost:7020/api/Product")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        return response.json();
+      })
+      .then((data) => setProducts(data)) // loading succesfully
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false)); // loading unsuccessfully
   }, []);
 
   return (
-    <div>
-      <ProductList products={products} />
-    </div>
+    <>
+      <Navbar />
+      <Container sx={{ marginTop: "80px" }}>
+        {loading ? (
+          <p>Error loading products...</p>
+        ) : (
+          <ProductList products={products} />
+        )}
+      </Container>
+    </>
   );
 }
--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,37 +1,13 @@
-import { Container } from "@mui/material";
-import ProductList from "../features/products/ProductList";
 import Navbar from "./Navbar";
-import type { Product } from "../models/product";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://localhost:7020/api/Product")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch products");
-        }
-        return response.json();
-      })
-      .then((data) => setProducts(data)) // loading succesfully
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false)); // loading unsuccessfully
-  }, []);
 
   return (
-    <>
+
+      <CssBaseline></CssBaseline>
       <Navbar />
-      <Container sx={{ marginTop: "80px" }}>
-        {loading ? (
-          <p>Error loading products...</p>
-        ) : (
-          <ProductList products={products} />
-        )}
-      </Container>
-    </>
+      <Outlet />
+      
   );
 }
 
