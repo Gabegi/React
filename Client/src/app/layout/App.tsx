686445c3aef18a7c5a820c25fb01b194ab72ce42
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-04 13:21:02 +0500

    routing

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,13 +1,14 @@
+import { CssBaseline } from "@mui/material";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function App() {
-
   return (
-
-      <CssBaseline></CssBaseline>
+    <>
+      <CssBaseline />
       <Navbar />
       <Outlet />
-      
+    </>
   );
 }
 
